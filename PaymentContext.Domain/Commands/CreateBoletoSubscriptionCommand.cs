dba5de9ea98b47f7cf6ebd5ba6b73b1b6b0d8863
Douglas Mariano Silverio <douglas.silverio@sistemapoliedro.com.br>
2018-10-01 11:37:02 -0300

    aula 15

---
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -0,0 +1,33 @@
+using System;
+using PaymentContext.Domain.Enums;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateBoletoSubscriptionCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; private set; }
+        public string Email { get; set; }
+        public string Barcode { get; private set; }
+        public string BoletoNumber { get; private set; }
+        public string PaymentNumber { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public Decimal Total { get; set; }
+        public Decimal TotalPaid { get; set; }
+        public string Payer { get; set; }
+        public string PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public string PayerEmail { get; set; }
+
+    }
+
+}
